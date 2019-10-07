95ea37a99d8699c0d30df9d44eaffe7ba455fa3e
BranceA <brancealexander@gmail.com>
2019-10-07 16:54:42 -0500

    adding student and studenttest with junit

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,7 +1,42 @@
+import java.util.ArrayList;
+
 public class Student {
 
-    public static void main(String[] args) {
-        System.out.println("Test");
+    private long id;
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(String name, long id) {
+        this.id = id;
+        this.name = name;
+        this.grades = new ArrayList<Integer>();
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addGrade(int grade){
+        grades.add(grade);
+    }
+
+    public int getGradeAverage(){
+        int total = 0;
+        for(int i = 0; i < grades.size(); i++){
+            total += grades.get(i);
+        }
+        return total/grades.size();
+    }
 }
--- src/main/java/StudentTest.java
+++ src/main/java/StudentTest.java
@@ -1,2 +1,28 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
 public class StudentTest {
+    @Before
+    public void setup(){
+        testingDude.addGrade(90);
+        testingDude.addGrade(80);
+    }
+
+    public Student testingDude = new Student("Brance", 664);
+
+    @Test
+    public void testingIfAStudentIsMadeCorrectly(){
+        assertEquals("Brance", testingDude.getName());
+        assertEquals(664, testingDude.getId());
+    }
+
+    @Test
+    public void testingGradeAverage(){
+        assertEquals(85, testingDude.getGradeAverage());
+    }
+
 }
